d0458b656e053bdb9cf58b0ede32148ff51b02f7
MRWH1TE117 <121175632+MRWH1TE117@users.noreply.github.com>
2025-11-25 16:04:16 +0100

    updated functions logic

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let count = 0;
+let countEl = document.getElementById("count-el");
 
 function increment() {
   count = count + 1;
-  document.getElementById("count-el").innerText = count;
-  console.log(count);
+  countEl.innerText = count;
 }
 
 function reset() {
-  document.getElementById("count-el").innerText = 0;
+  countEl.innerText = 0;
   count = 0;
-  console.log(count);
 }
