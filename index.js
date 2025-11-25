af5f3fb6fbff1c4eceaeda6bcfc26d1552850165
MRWH1TE117 <121175632+MRWH1TE117@users.noreply.github.com>
2025-11-25 13:54:56 +0100

    updated counter function and added reset

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+let count = 0;
+
 function increment() {
-  console.log("The INCREMENT button was clicked");
+  count = count + 1;
+  document.getElementById("count-el").innerText = count;
+  console.log(count);
+}
+
+function reset() {
+  document.getElementById("count-el").innerText = 0;
+  count = 0;
+  console.log(count);
 }
